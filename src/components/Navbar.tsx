f3cb0d4b4abe65e9882a6c9d5894fc87d16ab6cb
Prasath <prasathkanishk407@gmail.com>
2025-03-29 13:33:13 +0530

    Synced Clerk User to Postgres Db Users Table

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,8 +1,12 @@
 import Link from "next/link";
 import DesktopNavbar from "./DesktopNavbar";
 import MobileNavbar from "./MobileNavbar";
+import { currentUser } from "@clerk/nextjs/server";
+import { syncUser } from "@/actions/user.action";
 
-function Navbar() {
+async function Navbar() {
+  const user = await currentUser();
+  if(user) await syncUser();
 
   return (
     <nav className="sticky top-0 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60 z-50">
